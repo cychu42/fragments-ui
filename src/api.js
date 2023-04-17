340cdfb7dcd75c60659a7a4590cd44f19af8178a
cchu42 <cchu42@myseneca.ca>
2023-04-16 21:44:58 -0400

    finish CRUD

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ const apiUrl = process.env.API_URL || 'http://localhost:8080';
  * to have an `idToken` attached, so we can send that along with the request.
  */
 export async function getUserFragments(user) {
-  console.log('Requesting user fragments data...');
+  console.log('Requesting user fragments metadata...');
   try {
     const res = await fetch(`${apiUrl}/v1/fragments?expand=1`, {
       // Generate headers with the proper Authorization bearer token to pass
@@ -25,11 +25,90 @@ export async function getUserFragments(user) {
   }
 }
 
+export async function getByIdFragments(user, id) {
+  // Whether to set innerHTML
+  let setHtml = true;
+
+  console.log('Requesting user fragments data...');
+
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      // Generate headers with the proper Authorization bearer token to pass
+      headers: user.authorizationHeaders(),
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    console.log(res.headers.get('content-type'));
+    let data;
+
+    // Extra data by content-type
+    if (
+      res.headers.get('content-type').match(/text\/\b/i) ||
+      res.headers.get('content-type') === 'application/json'
+    ) {
+      data = await res.text();
+    } else {
+      // Expect image/* type and turn the body into a blob
+      data = await res.blob();
+
+      // Make an url for the blob
+      const url = URL.createObjectURL(data);
+      console.log(url);
+
+      // Create image element and set the src
+      const img = document.createElement('img');
+      img.src = url.split('blob:')[1];
+
+      // Clear, append the img element, and clean up if needed.
+      const showImg = document.getElementById('result');
+      showImg.innerHTML = '';
+      if (showImg.children.length > 0) {
+        showImg.removeChild(showImg.children[0]);
+      }
+      showImg.appendChild(img);
+
+      // No need to be set innerHTML
+      setHtml = false;
+    }
+
+    // Set innerHTML
+    if (setHtml === true) {
+      document.getElementById('result').innerHTML = data;
+    }
+  } catch (err) {
+    console.error('Unable to call GET /v1/fragment/:id', { err });
+    return err;
+  }
+}
+
 export async function postFragment(user, fragmentData, type) {
   console.log('Creating a user fragment...');
   try {
     const res = await fetch(`${apiUrl}/v1/fragments`, {
       method: 'POST',
+      // Generate headers with the proper Authorization bearer token to pass
+      headers: user.authorizationHeaders(type),
+      body: fragmentData,
+    });
+
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log('Got the user fragment data back after POST', { data });
+    return JSON.stringify(data);
+  } catch (err) {
+    console.error('Unable to call POST /v1/fragments', { err });
+    return err;
+  }
+}
+
+export async function updateFragment(user, fragmentData, type, id) {
+  console.log('Creating a user fragment...');
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      method: 'PUT',
       // Generate headers with the proper Authorization bearer token to pass
       headers: user.authorizationHeaders(type),
       body: fragmentData,
@@ -61,10 +140,10 @@ export async function deleteFragment(user, id) {
     }
     const data = await res.json();
 
-    console.log('Got the user fragment data back after POST', { data });
+    console.log('Reponse after DELETE is', { data });
     return JSON.stringify(data);
   } catch (err) {
-    console.error('Unable to call POST /v1/fragments', { err });
+    console.error('Unable to call DELETE /v1/fragments/:id', { err });
     return err;
   }
 }
--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 import { Auth, getUser } from './auth';
-import { getUserFragments, postFragment, deleteFragment } from './api';
+import {
+  getUserFragments,
+  postFragment,
+  deleteFragment,
+  getByIdFragments,
+  updateFragment,
+} from './api';
 
 async function init() {
   // Get our UI elements
   const userSection = document.querySelector('#user');
   const loginBtn = document.querySelector('#login');
   const logoutBtn = document.querySelector('#logout');
-  const postBtn = document.querySelector('#post');
-  const getBtn = document.querySelector('#get');
-  const deleteBtn = document.querySelector('#delete');
+  const postTextBtn = document.querySelector('#postTextBtn');
+  const postImgBtn = document.querySelector('#postImgBtn');
+  const getBtn = document.querySelector('#getBtn');
+  const getByIdBtn = document.querySelector('#getByIdBtn');
+  const updateTextBtn = document.querySelector('#updateTextBtn');
+  const updateImgBtn = document.querySelector('#updateImgBtn');
+  const deleteBtn = document.querySelector('#deleteBtn');
+
   // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
     // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
@@ -41,8 +52,8 @@ async function init() {
   // Disable the Login button
   loginBtn.disabled = true;
 
-  // Handle post button click
-  postBtn.onclick = async () => {
+  // Handle post text button click
+  postTextBtn.onclick = async () => {
     const text = document.getElementById('postText').value;
     const type = document.getElementById('postType').value;
 
@@ -50,11 +61,49 @@ async function init() {
     document.getElementById('result').innerHTML = await postFragment(user, text, type);
   };
 
+  // Handle post image button click
+  postImgBtn.onclick = async () => {
+    let image = document.getElementById('postImg').files[0];
+    console.log(image);
+    const type = document.getElementById('postType').value;
+
+    // post request to create a fragment
+    document.getElementById('result').innerHTML = await postFragment(user, image, type);
+  };
+
   //  Handle get button click
   getBtn.onclick = async () => {
     // Do an authenticated request to the fragments API server and get the result
     // Get user's existing fragments' metadata
     document.getElementById('result').innerHTML = await getUserFragments(user);
+  };
+
+  //  Handle getByIdBtn button click
+  getByIdBtn.onclick = async () => {
+    const id = document.getElementById('getId').value;
+    // Do an authenticated request to the fragments API server and get the result
+    // Get the existing fragment's data
+    await getByIdFragments(user, id);
+  };
+
+  //  Handle update text button click
+  updateTextBtn.onclick = async () => {
+    const text = document.getElementById('updateText').value;
+    const type = document.getElementById('updateType').value;
+    const id = document.getElementById('updateId').value;
+
+    // Do an authenticated request to the fragments API server and get the result
+    document.getElementById('result').innerHTML = await updateFragment(user, text, type, id);
+  };
+
+  // Handle update image button click
+  updateImgBtn.onclick = async () => {
+    const image = document.getElementById('updateImg').value;
+    const type = document.getElementById('updateType').value;
+    const id = document.getElementById('updateId').value;
+
+    // post request to create a fragment
+    document.getElementById('result').innerHTML = await postFragment(user, image, type, id);
   };
 
   //  Handle get button click
